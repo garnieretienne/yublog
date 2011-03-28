504d84430ff718f97ab4461ee68736df5c8100d6
Etienne Garnier (kurt/yuweb) <garnier.etienne@gmail.com>
2011-03-28 11:27:12 -0700

    use config file for application options

--- app.rb
+++ app.rb
@@ -5,15 +5,33 @@ require 'haml'
 module Blog
   class Application < Sinatra::Application
 
+    configure do
+      # Defaults values
+      set :configs     => 'config'
+      set :base        => 'posts'
+      set :name        => 'Blog'
+      set :description => 'Blog engine'
+
+      # Read config file
+      if File.exist?(settings.configs + "/config.yml")
+        cfg = YAML.load_file( settings.configs + "/config.yml")
+        cfg.each_pair do |key, value|
+          set(key.to_sym, value) if value != nil
+        end
+      end
+    end
+
     # root, posts index
     get '/' do
-      @posts = Post.all('posts')
+      @title = 'Welcome'
+      @posts = Post.all(settings.base)
       haml :index
     end
 
     # view a blog post
     get '/:year/:month/:day/:title' do
-      @post = Post.new('posts/' + "#{params[:year]}-#{params[:month]}-#{params[:day]}-#{params[:title]}.md")
+      @post = Post.new(settings.base + '/' + "#{params[:year]}-#{params[:month]}-#{params[:day]}-#{params[:title]}.md")
+      @title = @post.title
       haml :view
     end
   end
