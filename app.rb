4f8e084044a16c3ef0388c73330f59e40b4b91a8
Etienne Garnier (kurt/yuweb) <garnier.etienne@gmail.com>
2011-03-27 23:09:07 -0700

    view a blog post writed in markdown with a dated route url and a post view template

--- app.rb
+++ app.rb
@@ -5,11 +5,16 @@ require 'haml'
 module Blog
   class Application < Sinatra::Application
 
-    # root
+    # root, posts index
     get '/' do
       @posts = Post.all('posts')
       haml :index
     end
 
+    # view a blog post
+    get '/:year/:month/:day/:title' do
+      @post = Post.new('posts/' + "#{params[:year]}-#{params[:month]}-#{params[:day]}-#{params[:title]}.md")
+      haml :view
+    end
   end
 end
