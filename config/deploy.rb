50d4299feb048dd4c22e41ac117a3fe5465c5740
Etienne Garnier <garnier.etienne@gmail.com>
2011-04-21 01:34:00 +0200

    update capistrano deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :scm, :git
 set :repository,  "https://github.com/garnieretienne/yublog.git"
 set :branch, 'develop'
 set :deploy_via, :remote_cache
+default_run_options[:pty] = true # sometimes needed for some distros
 
 # Roles
 server "95.142.169.122", :app, :web
@@ -22,7 +23,8 @@ set :use_sudo, true
 
 # RVM and Bundler options
 set :rvm_ruby_string, "#{ruby}@#{application}"
-set :rvm_type, :user
+set :sudo, "PATH=/sbin:/usr/sbin:$PATH && /usr/local/bin/rvmsudo" # bind sudo command with RVM and enable admin commands
+set :sudo_prompt, ''
 set :bundle_dir, "" # install gems in the dedicated gemset
 set :bundle_flags, "--quiet"
 
@@ -43,7 +45,6 @@ namespace:deploy do
 	# Clean these tasks
   task :cold do
 	  deploy.update
-		deploy.start
 	end
 	# Disable these tasks
 	task :migrate do
@@ -55,13 +56,26 @@ end
 # Define shared folder here
 namespace :shared do
   # if posts are stored in app/posts
-  desc "Store the post folder in shared space"
-	task :posts do
-	  run("mkdir -p #{deploy_to}/shared/posts")
-    run("ln -nfs #{deploy_to}/shared/posts #{deploy_to}/current/")
+  desc "Restore the post folder from shared space in current"
+	task :link_posts do
+    run("if [[ -d #{deploy_to}/shared/posts ]]; then ln -nfs #{deploy_to}/shared/posts #{deploy_to}/current/; fi")
+	end
+	desc "Save the post folder"
+	task :save_posts do
+		run("if [[ -d #{deploy_to}/current/posts && ! -d #{deploy_to}/shared/posts ]]; then mv #{deploy_to}/current/posts #{deploy_to}/shared/; fi")
+	end
+	desc "Keep the config file"
+	task :config do
+	  run("if [[ ! -f #{deploy_to}/shared/config.yml ]]; then cp #{deploy_to}/current/config/config.yml #{deploy_to}/shared/; fi")
+	  run("ln -nfs #{deploy_to}/shared/config.yml #{deploy_to}/current/config/")
 	end
 	# link shared tasks with deploy:update_code
-  after "deploy:update_code", "shared:posts"
+  after "deploy:symlink" do
+	  "init:setup" # give write right on new folder
+	  link_posts
+		config
+	end
+	before "deploy:symlink", "init:setup", "shared:save_posts" # give write right on new folder
 end
 
 # Define init tasks here
@@ -84,29 +98,36 @@ namespace :init do
 	# RVM configuration
 	desc "Create RVM gemset for this application"
 	task :rvm, :role => :app do
-	  sudo("rvm gemset create #{application}")
-		run("echo 'rvm use #{ruby}@#{application} > /dev/null' > #{deploy_to}/.rvmrc")
-		run("rvm rvmrc trust #{deploy_to}/.rvmrc")
+	  run("rvm gemset create #{application}")
+		run("rvm list gemsets")
 	end
+	desc "Create .rvmrc file for this application"
+	  task :rvmrc, :role => :app do
+		run("echo 'rvm use #{ruby}@#{application} > /dev/null' > #{deploy_to}/.rvmrc")
+    run("rvm rvmrc trust #{deploy_to}/.rvmrc")
+  end
 	# Thin configuration
 	desc "Create a thin config and service files"
 	task :thin, :role => :app do
 	  sudo("mkdir -p /etc/thin/#{application}")
 		sudo("mkdir -p /var/log/thin/")
-	  sudo("thin -c #{deploy_to}/current -S /tmp/#{application}.sock -l /var/log/thin/#{application}.log -s 1 -C /etc/thin/#{application}/#{application}.yml -e profuction config")
+	  sudo("thin -c #{deploy_to}/current -S /tmp/#{application}.sock -l /var/log/thin/#{application}.log -s 1 -C /etc/thin/#{application}/#{application}.yml -e production config")
 		sudo("thin install")
 		sudo("mv /etc/init.d/thin /etc/init.d/#{application}")
-		sudo("sudo rvm wrapper #{ruby}@#{application} #{application} thin") # create /usr/local/bin/app_thin
-		sudo("sed -i 's/DAEMON=\\/usr\\/bin\\/thin/DAEMON=\\/usr\\/local\\/bin\\/#{application}_thin/g' /etc/init.d/#{application}")
-		sudo("sed -i 's/SCRIPT_NAME=\\/etc\\/init.d\\/thin/SCRIPT_NAME=\\/etc\\/init.d\\/#{application}/g' /etc/init.d/#{application}")
-		sudo("sed -i 's/CONFIG_PATH=\\/etc\\/thin/CONFIG_PATH=\\/etc\\/thin\\/#{application}/g' /etc/init.d/#{application}")
-		sudo("update-rc.d #{application} defaults")
+		sudo("rvm wrapper #{ruby}@#{application} #{application} thin") # create /usr/local/bin/app_thin
+		sudo("sed -i 's#DAEMON=.*#DAEMON=/usr/local/bin/#{application}_thin#g' /etc/init.d/#{application}")
+		sudo("sed -i 's#SCRIPT_NAME=.*#SCRIPT_NAME=/etc/init.d/#{application}#g' /etc/init.d/#{application}")
+		sudo("sed -i 's#CONFIG_PATH=.*#CONFIG_PATH=/etc/thin/#{application}#g' /etc/init.d/#{application}")
+		sudo("/usr/sbin/update-rc.d #{application} defaults")
 	end
 	# link init tasks with deploy:setup
+	before "deploy:setup" do
+	  rvm
+	end
   after "deploy:setup" do
     setup
     nginx
-    rvm
+		rvmrc
     thin
   end
 end
