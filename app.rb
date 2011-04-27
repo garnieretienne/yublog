aa6730b54d4bbb5b09002ed6bac2e8329c37bfa1
Etienne Garnier <garnier.etienne@gmail.com>
2011-04-28 01:43:15 +0200

    fix disqus comments and enable debugging mode

--- app.rb
+++ app.rb
@@ -85,16 +85,18 @@ module Blog
       end
       # Show comments if disqus is enabled
       if settings.disqus['shortname']!=nil then
-        @disqus = %{<script type="text/javascript">
+        @disqus = %{<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
     var disqus_shortname = '#{settings.disqus['shortname']}';
     var disqus_identifier = '#{filename}';
+    var disqus_developer = '#{if settings.disqus['debug'] then '1'; else '0'; end}';
 
-    (function () {
-        var s = document.createElement('script'); s.async = true;
-        s.type = 'text/javascript';
-        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-    }());
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
